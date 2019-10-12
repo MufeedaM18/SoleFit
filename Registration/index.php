73d07821dbe1d5872b697da5fa89757f2e5fb4fa
Ashwin Joy <ashwinparekkudiyil@gmail.com>
2019-10-12 12:48:10 +0530

    Add files via upload

---
+++ Registration/index.php
@@ -0,0 +1,169 @@
+<?php 
+  session_start(); 
+
+  if (!isset($_SESSION['username'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['username']);
+  	header("location: login.php");
+  }
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" media="screen" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.min.css">
+
+	<title>Sole-Fit | My Account</title>
+     
+      <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+   
+       <link rel="stylesheet" href="css/animate.css">
+
+      <link rel="stylesheet" href="css/login.css">
+
+</head>
+
+<body>
+
+    
+   <!-- Navigation -->
+    <nav id="mainNav" class="navbar navbar-default navbar-custom navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll animated infinite bounce" id="solefit" href="#page-top">Sole-Fit</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li id="home">
+                        <a class="page-scroll" href="../Home/index.php"><i class="fa fa-home" aria-hidden="true"></i>
+ Home</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="../Blog/blog.php"><i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+ Blog</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="../FoodSwaps/foodswaps.php"><i class="fa fa-coffee " aria-hidden="true"></i>
+ FoodSwaps</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="../Dietplan/dietplans.php"><i class="fa fa-spoon" aria-hidden="true"></i>
+ DietPlans</a>
+                    </li>
+                    
+                     <li>
+                        <a class="page-scroll" href="../Dr.Fit/drfit.php"><i class="fa fa-user-md " aria-hidden="true"></i>
+ Dr.Fit</a>
+                    </li>
+                    
+                     <li>
+                        <a class="page-scroll" href="../Fitspots/fitspots.php"><i class="fa fa-hospital-o " aria-hidden="true"></i>
+FitSpots</a>
+                    </li>
+                    
+                    <li>
+                        <a class="page-scroll"  href="../Contact/contact.php"><i class="fa fa-phone" aria-hidden="true"></i>
+ Contact</a>
+                    </li> 
+                     <li id="logz">    
+                       <a class="page-scroll"  href="../Registration/index.php"><i class="fa fa-smile-o" aria-hidden="true"></i>
+                                     <?php  if (isset($_SESSION['username'])) : 
+                                               echo $_SESSION['username'];
+                                            else :    
+                                                echo "Login";
+                                            endif ?>
+                        </a>
+                  </li>
+             
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+  
+  
+  
+  
+   <div class="logfit jumbotron animated fadeInDown" id="top" style="height:300px;width:300px;">
+     
+
+    <div>
+        <a href="index.php?logout='1'" class="btn btn-danger text-center" style="color: #fff;">Log Out</a> 
+    </div>
+    
+     
+   </div>
+
+<br>
+
+
+<style>
+    
+    @media (max-width: 400px){
+        .btn-danger{
+            margin-left: 100px;
+            margin-top: 120px;
+        }
+    }
+    
+</style>
+
+
+ <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <span class="copyright">&copy; 2018 Sole-Fit</span>
+                </div>
+                <div class="col-md-4">
+                   
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    
+
+     <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    
+
+</body>
+
+</html>
---
+++ Registration/register.php
@@ -0,0 +1,156 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" media="screen" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.min.css">
+
+	<title>Sole-Fit | Sign Up</title>
+     
+      <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+   
+       <link rel="stylesheet" href="css/animate.css">
+
+      <link rel="stylesheet" href="css/signup.css">
+
+</head>
+
+<body>
+
+    
+   <!-- Navigation -->
+    <nav id="mainNav" class="navbar navbar-default navbar-custom navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll animated infinite bounce" id="solefit" href="#page-top">Sole-Fit</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li id="home">
+                        <a class="page-scroll" href="../Home/index.php"><i class="fa fa-home" aria-hidden="true"></i>
+ Home</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="../Blog/blog.php"><i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+ Blog</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="../FoodSwaps/foodswaps.php"><i class="fa fa-coffee " aria-hidden="true"></i>
+ FoodSwaps</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="../Dietplan/dietplans.php"><i class="fa fa-spoon" aria-hidden="true"></i>
+ DietPlans</a>
+                    </li>
+                    
+                     <li>
+                        <a class="page-scroll" href="../Dr.Fit/drfit.php"><i class="fa fa-user-md " aria-hidden="true"></i>
+ Dr.Fit</a>
+                    </li>
+                    
+                     <li>
+                        <a class="page-scroll" href="../Fitspots/fitspots.php"><i class="fa fa-hospital-o " aria-hidden="true"></i>
+FitSpots</a>
+                    </li>
+                    
+                    <li>
+                        <a class="page-scroll"  href="../Contact/contact.php"><i class="fa fa-phone" aria-hidden="true"></i>
+ Contact</a>
+                    </li>
+                                <li id="logz">    
+                       <a class="page-scroll"  href="../Registration/index.php"><i class="fa fa-smile-o" aria-hidden="true"></i>
+                                     <?php  if (isset($_SESSION['username'])) : 
+                                               echo $_SESSION['username'];
+                                            else :    
+                                                echo "Login";
+                                            endif ?>
+                        </a>
+                  </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+  
+  
+  
+  
+   <div class="logfit jumbotron animated fadeInDown">
+       <form method="post" action="register.php">
+              	<?php include('errors.php'); ?>
+            <h3 class="mytyped">Create your account. <i class="fa fa-smile-o animated bounce" aria-hidden="true"></i>
+            </h3> <br>
+            
+           <input type="text" placeholder=" Username" name="username" value="<?php echo $username; ?>" required data-validation-required-message="Please enter your name."><br> <br>
+           <input type="email" placeholder=" Email ID" name="email" value="<?php echo $email; ?>" required data-validation-required-message="Please enter your email address."><br> <br>
+            <input type="password" placeholder=" password" name="password_1"><br><br>
+            <input type="password" placeholder=" retype password" name="password_2"><br><br>
+            <button class="btn  mybutton" name="reg_user" >Sign Up</button><br><br>
+            <a href="login.php">Already have an account? Log In Here!</a>
+            
+       </form>
+   </div>
+   
+   
+   
+   
+   
+
+<br><br><br><br>
+
+
+
+ <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <span class="copyright">&copy; 2018 Sole-Fit</span>
+                </div>
+                <div class="col-md-4">
+                    
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    
+
+     <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    
+	<script src="js/login.js">
+	</script>
+
+</body>
+
+</html>
